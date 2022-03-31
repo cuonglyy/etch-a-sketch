f817f3e800b4f1fb2e1e8d6f31d1e2a13c2a080f
Cuong Ly <cuonglyy@gmail.com>
2022-03-31 12:35:25 -0700

    Clear button functional

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_GRID = 16;
+
+const currentGrid = DEFAULT_GRID;
+const currentColor = DEFAULT_COLOR;
+
+const clearBtn = document.querySelector("#clear-btn");
 const gridContainer = document.querySelector("#grid-container");
 
+let changeGrid = (newGrid) => {
+  currentGrid = newGrid;
+}
+
+let changeColor = (newColor) => {
+  currentColor = newColor;
+}
+
+let addColor = (e) => {
+  e.target.style.backgroundColor = currentColor;
+}
+
+// Removes all elements inside grid container and remakes the grid in new size
+let newGrid = () => {
+  gridContainer.innerHTML = '';
+  makeGrid(currentGrid);
+}
 
 // Use inline style to create css grids
 let makeGrid = (gridNum) => {
@@ -11,12 +35,15 @@ let makeGrid = (gridNum) => {
   //Append grid elements into the container n^2 times
   for (let i = 0; i < gridNum * gridNum; i++) {
     //Create grid element and add class attribute named "grid-item"
-    const grid = document.createElement("div");
-    grid.setAttribute("class", "grid-item")
-    gridContainer.appendChild(grid);
-
+    const gridElement = document.createElement("div");
+    gridElement.setAttribute("class", "grid-item")
+    gridElement.addEventListener("mouseenter", addColor);
+    gridContainer.appendChild(gridElement);
   }
-
 }
 
-makeGrid(16);
+clearBtn.addEventListener("click", newGrid);
+
+window.onload = () => {
+  makeGrid(DEFAULT_GRID);
+}
